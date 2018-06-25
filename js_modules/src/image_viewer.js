f23ee16815f80abde84e46bc509276e40d6a3397
nigel-riss <mr.kurenkov@gmail.com>
2018-06-25 16:31:37 +0200

    Add image_loader module

---
+++ js_modules/src/image_viewer.js
@@ -0,0 +1,4 @@
+const image = document.createElement('img');
+// image.src = 'http://via.placeholder.com/400x400';
+image.src = 'https://placeimg.com/400/400/any';
+document.body.appendChild(image);
