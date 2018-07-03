3d244883ad1da8594fa0de0aa3bd4589bee901bd
nigel-riss <mr.kurenkov@gmail.com>
2018-07-03 11:49:44 +0200

    Add image_viever import

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,15 +1,18 @@
-import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 
 import '../styles/image_viewer.css';
 import '../styles/test.css';
 
-const image = document.createElement('img');
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
+    document.body.appendChild(image);
+}
+
 // image.src = 'http://via.placeholder.com/400x400';
 // image.src = 'https://placeimg.com/400/400/any';
-image.src = small;
-document.body.appendChild(image);
 
-const bigImage = document.createElement('img');
-bigImage.src = big;
-document.body.appendChild(bigImage);
+// import big from '../assets/big.jpg';
+// const bigImage = document.createElement('img');
+// bigImage.src = big;
+// document.body.appendChild(bigImage);
